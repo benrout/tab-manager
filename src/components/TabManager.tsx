27226a5b359b147b6fd610fa03f5920268c39285
Ben Rout <ben.rout@sysdocgroup.com>
2019-12-02 15:31:02 +0000

    Updates TabManager to set isLoading state in try/catch as finally is called before try is complete (setState is asynchronous)

--- src/components/TabManager.tsx
+++ src/components/TabManager.tsx
@@ -34,21 +34,21 @@ export class TabManager extends React.Component<{}, TabManagerState> {
         try {
             this.setState({
                 tabs: [
-                    { id: 1, title: 'home', content: 'home content' },
-                    { id: 2, title: 'contact', content: 'contact content' },
-                    { id: 3, title: 'about', content: 'about content' }
-                ]
+                    { id: 1, title: 'Home', content: 'home content' },
+                    { id: 2, title: 'Contact', content: 'contact content' },
+                    { id: 3, title: 'About', content: 'about content' }
+                ],
+                isLoading: false
             }, () => {
-                this.setState({
-                    selectedTab: this.state.tabs[0]
-                });
+                if (this.state.tabs[0]) {
+                    this.setState({
+                        selectedTab: this.state.tabs[0]
+                    });
+                }
             });
         } catch (error) {
             this.setState({
-                error
-            });
-        } finally {
-            this.setState({
+                error: "Couldn't load tabs",
                 isLoading: false
             });
         }
@@ -56,14 +56,17 @@ export class TabManager extends React.Component<{}, TabManagerState> {
 
     handleTabClick = (id: number) => {
         const selectedTab: ITab = this.state.tabs.filter(tab => tab.id === id)[0];
-        this.setState({
-            selectedTab
-        });
+
+        if (selectedTab) {
+            this.setState({
+                selectedTab
+            });
+        };
     }
 
     render() {
         const { isLoading, tabs, error, selectedTab } = this.state;
-        
+
         return (
             <div>
                 {isLoading ?
